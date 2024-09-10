a42e8fc2f93252559db491a3a8e9bce4d819c0ef
Muhammed Ali Cinar <muhammed.ali.cinar@outlook.com>
2024-09-10 16:59:57 +0200

    Improved

--- basic.py
+++ basic.py
@@ -1,10 +1,6 @@
 # Parent Class
 class Animal:
-    def __init__(self, name):
-        self.name = name
-    
-    def speak(self):
-        return f"{self.name} makes a sound"
+    ...
 
 # Child Class
 class Dog:
